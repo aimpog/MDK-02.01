47b81e1af687526609f2ff1573af823013bc7e7e
aimpog <145752956+aimpog@users.noreply.github.com>
2023-09-22 11:30:04 +0300

    22.09.2023

---
+++ PROG2.cs
@@ -0,0 +1,22 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int SumAllDigit(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        int digit = number % 10; // Получаем последнюю цифру числа.
+        result += digit; // Добавляем цифру к результату.
+        number /= 10; // Убираем последнюю цифру из числа.
+    }
+    return result;
+}
+
+int number = Prompt("Введите число:");
+System.Console.WriteLine($"Сумма всех чисел в цифре {number} = {SumAllDigit(number)}");
+
